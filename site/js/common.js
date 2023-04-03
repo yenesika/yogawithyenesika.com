43a8aaffc8f480681ebccf696847d73c16c90bbe
Thoshith <thoshith@yahoo.com>
2023-04-03 20:02:20 +0530

    Update js

--- site/js/common.js
+++ site/js/common.js
@@ -1,122 +1,70 @@
 document.addEventListener("DOMContentLoaded", function () {
-  'use strict';
-  /* =======================
-  // Animation Load Page
-  ======================= */
-  var body = document.querySelector("body");
-  setTimeout(function(){
-    body.classList.add("is-in");
-  },150)
-
-  /* ==================================
-  // Stop Animations After All Have Run
-  ================================== */
-  setTimeout(function(){
-    body.classList.add("stop-animations");
-  },1500)
-
-  /* ======================================
-  // Stop Animations During Window Resizing
-  ====================================== */
-  let resizeTimer;
-  window.addEventListener("resize", () => {
-    document.body.classList.add("resize-animation-stopper");
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(() => {
-      document.body.classList.remove("resize-animation-stopper");
-    }, 300);
-  });
-
-
-  /* =======================
-  // Responsive Videos
-  ======================= */
-  reframe(".post__content iframe:not(.reframe-off), .page__content iframe:not(.reframe-off)");
-
-
-  /* =======================
-  // Zoom Image
-  ======================= */
-  const lightense = document.querySelector(".page img, .post img"),
-  imageLink = document.querySelectorAll(".page a img, .post a img");
-
-  if (imageLink) {
-    for (var i = 0; i < imageLink.length; i++) imageLink[i].parentNode.classList.add("image-link");
-    for (var i = 0; i < imageLink.length; i++) imageLink[i].classList.add("no-lightense");
-  }
-
-  if (lightense) {
-    Lightense(".page img:not(.no-lightense), .post img:not(.no-lightense)", {
-    padding: 60,
-    offset: 30
+    "use strict";
+    var o = document.querySelector("body"), r = document.querySelector(".nav__icon-menu"),
+        a = document.querySelector(".nav__icon-close"), i = document.querySelector(".main-nav");
+    r.addEventListener("click", () => {
+        c()
+    }), a.addEventListener("click", () => {
+        d()
     });
-  }
 
-  /* ============================
-  // Testimonials Slider
-  ============================ */
-  if (document.querySelector(".my-slider")) {
-    var slider = tns({
-      container: ".my-slider",
-      items: 3,
-      slideBy: 1,
-      gutter: 20,
-      nav: false,
-      mouseDrag: true,
-      autoplay: false,
-      controlsContainer: "#customize-controls",
-      responsive: {
-        1024: {
-          items: 3,
-        },
-        768: {
-          items: 2,
-        },
-        0: {
-          items: 1,
-        }
-      }
-    });
-  }
-
-
-  /* ============================
-  // iTyped
-  ============================ */
-  if (document.querySelector(".c-subscribe")) {
-    var options = {
-      strings: itype_text,
-      typeSpeed: 100,
-      backSpeed: 50,
-      startDelay: 200,
-      backDelay: 1500,
-      loop: true,
-      showCursor: true,
-      cursorChar: "|",
-      onFinished: function(){}
+    function c() {
+        i.classList.add("is-open")
     }
 
-    ityped.init('#ityped', options);
-  }
-
-
-  /* ============================
-  // Scroll to top
-  ============================ */
-  const btnScrollToTop = document.querySelector(".top");
-
-  window.addEventListener("scroll", function () {
-    window.scrollY > window.innerHeight ? btnScrollToTop.classList.add("is-active") : btnScrollToTop.classList.remove("is-active");
-  });
-
-  btnScrollToTop.addEventListener("click", function () {
-    if (window.scrollY != 0) {
-      window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-      })
+    function d() {
+        i.classList.remove("is-open")
     }
-  });
 
+    setTimeout(function () {
+        o.classList.add("is-in")
+    }, 150), setTimeout(function () {
+        o.classList.add("stop-animations")
+    }, 1500);
+    let s;
+    window.addEventListener("resize", () => {
+        document.body.classList.add("resize-animation-stopper"), clearTimeout(s), s = setTimeout(() => {
+            document.body.classList.remove("resize-animation-stopper")
+        }, 300)
+    }), reframe(".post__content iframe:not(.reframe-off), .page__content iframe:not(.reframe-off)");
+    const l = document.querySelector(".page img, .post img"), t = document.querySelectorAll(".page a img, .post a img");
+    if (t) {
+        for (var e = 0; e < t.length; e++) t[e].parentNode.classList.add("image-link");
+        for (var e = 0; e < t.length; e++) t[e].classList.add("no-lightense")
+    }
+    if (l && Lightense(".page img:not(.no-lightense), .post img:not(.no-lightense)", {
+        padding: 60,
+        offset: 30
+    }), document.querySelector(".my-slider")) var u = tns({
+        container: ".my-slider",
+        items: 3,
+        slideBy: 1,
+        gutter: 20,
+        nav: !1,
+        mouseDrag: !0,
+        autoplay: !1,
+        controlsContainer: "#customize-controls",
+        responsive: {1024: {items: 3}, 768: {items: 2}, 0: {items: 1}}
+    });
+    if (document.querySelector(".c-subscribe")) {
+        var m = {
+            strings: itype_text,
+            typeSpeed: 100,
+            backSpeed: 50,
+            startDelay: 200,
+            backDelay: 1500,
+            loop: !0,
+            showCursor: !0,
+            cursorChar: "|",
+            onFinished: function () {
+            }
+        };
+        ityped.init("#ityped", m)
+    }
+    const n = document.querySelector(".top");
+    window.addEventListener("scroll", function () {
+        window.scrollY > window.innerHeight ? n.classList.add("is-active") : n.classList.remove("is-active")
+    }), n.addEventListener("click", function () {
+        window.scrollY != 0 && window.scrollTo({top: 0, left: 0, behavior: "smooth"})
+    })
 });
